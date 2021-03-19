7fca3c8238aeaf1444f5530d6077ff4022dc66b7
Artur Shpontak <ashpontak97@gmail.com>
2021-03-19 12:51:35 +0200

    Updates

---
+++ precache-manifest.1706c9379aec908953e12190c4b72de3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5357ded9563ad11b5221ce14d16d6f38",
+    "url": "./index.html"
+  },
+  {
+    "revision": "71f7c6a7d7180b04dc23",
+    "url": "./static/css/main.819e4dd6.chunk.css"
+  },
+  {
+    "revision": "9bfabb54970d8a1edebf",
+    "url": "./static/js/2.c17dec48.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.c17dec48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71f7c6a7d7180b04dc23",
+    "url": "./static/js/main.775f65ec.chunk.js"
+  },
+  {
+    "revision": "89de80f3985b25002b5f",
+    "url": "./static/js/runtime-main.17b9eccd.js"
+  }
+]);
